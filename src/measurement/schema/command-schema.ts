ed540c6daf67586143b2d0017c17a3e2f4e07b0c
Artem Stoianov <zarianec91@gmail.com>
2022-03-18 15:56:53 +0100

    fix: update request validation rules (#30)
    * fix: update request validation rules
    
    * fix: move limits to config

--- src/measurement/schema/command-schema.ts
+++ src/measurement/schema/command-schema.ts
@@ -1,14 +1,14 @@
 import Joi from 'joi';
 
 export const pingSchema = Joi.object({
-	type: Joi.string().valid('ping'),
-	target: Joi.string(),
+	type: Joi.string().valid('ping').required(),
+	target: Joi.string().required(),
 	packets: Joi.number().min(1).max(16).default(3),
 });
 
 export const tracerouteSchema = Joi.object({
-	type: Joi.string().valid('traceroute'),
-	target: Joi.string(),
+	type: Joi.string().valid('traceroute').required(),
+	target: Joi.string().required(),
 	protocol: Joi.string().valid('TCP', 'UDP', 'ICMP').default('UDP'),
 	port: Joi.number().port().default(80),
 });
