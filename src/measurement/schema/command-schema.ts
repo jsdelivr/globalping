611e808d2b7cb606e313769b4a539c7398cd57ed
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-03-19 08:12:53 +0000

    Add: dns command

--- src/measurement/schema/command-schema.ts
+++ src/measurement/schema/command-schema.ts
@@ -12,3 +12,17 @@ export const tracerouteSchema = Joi.object({
 	protocol: Joi.string().valid('TCP', 'UDP', 'ICMP').default('UDP'),
 	port: Joi.number().port().default(80),
 });
+
+const allowedTypes = ['A', 'AAAA', 'ANY', 'CNAME', 'DNSKEY', 'DS', 'MX', 'NS', 'NSEC', 'PTR', 'RRSIG', 'SOA', 'TXT', 'SRV'];
+const allowedProtocols = ['UDP', 'TCP'];
+
+export const dnsSchema = Joi.object({
+	type: Joi.string().valid('dns').required(),
+	target: Joi.string().required(),
+	query: Joi.object({
+		type: Joi.string().valid(...allowedTypes).default('A'),
+		resolver: Joi.string(),
+		protocol: Joi.string().valid(...allowedProtocols).default('UDP'),
+		port: Joi.number().default('53'),
+	}).default({}),
+});
