e3585dd45ca66ab53e91878adf1099d04f3f9b53
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-03-20 02:02:24 +0000

    add: ratelimit mw access denied test

--- test/integration/ratelimit.test.ts
+++ test/integration/ratelimit.test.ts
@@ -1,16 +1,35 @@
 import type {Server} from 'node:http';
 import request, {Response} from 'supertest';
+import requestIp from 'request-ip';
+import type {RateLimiterRedis} from 'rate-limiter-flexible';
 import {expect} from 'chai';
 import {getOrInitTestServer} from '../utils/http.js';
 
 describe('rate limiter', () => {
 	let app: Server;
 	let requestAgent: any;
+	let clientIpv6: string;
+	let rateLimiterInstance: RateLimiterRedis;
 
 	before(async function () {
 		this.timeout(5000);
 		app = await getOrInitTestServer();
 		requestAgent = request(app);
+
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+		const httpResponse = await requestAgent.post('/v1/').send() as Response & {req: any};
+		// Supertest renders request as ipv4
+		const clientIp = requestIp.getClientIp(httpResponse.req)!;
+		// Koa sees ipv6-ipv4 monster
+		clientIpv6 = `::ffff:${clientIp}`;
+
+		// eslint-disable-next-line node/no-unsupported-features/es-syntax
+		const rateLimiter = await import('../../src/lib/ratelimiter.js');
+		rateLimiterInstance = rateLimiter.default;
+	});
+
+	afterEach(async () => {
+		await rateLimiterInstance.delete(clientIpv6);
 	});
 
 	describe('headers', () => {
@@ -44,6 +63,27 @@ describe('rate limiter', () => {
 
 			expect(responseList).to.have.lengthOf(5);
 			expect(firstResponse?.headers?.['x-ratelimit-remaining']).to.not.equal(lastResponse?.headers?.['x-ratelimit-remaining']);
+		});
+	});
+
+	describe('access', () => {
+		it('should succeed (limit not reached)', async () => {
+			await rateLimiterInstance.set(clientIpv6, 0, 0);
+
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+			const response = await requestAgent.post('/v1/').send() as Response;
+
+			expect(Number(response.headers['x-ratelimit-remaining'])).to.equal(99);
+		});
+
+		it('should fail (limit reached) (start at 100)', async () => {
+			await rateLimiterInstance.set(clientIpv6, 100, 0);
+
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+			const response = await requestAgent.post('/v1/').send() as Response;
+
+			expect(Number(response.headers['x-ratelimit-remaining'])).to.equal(0);
+			expect(response.statusCode).to.equal(429);
 		});
 	});
 });
