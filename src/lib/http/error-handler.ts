f3188a635a8217246d4fb60428551003dbb0456a
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-03-21 10:30:31 +0000

    ref: http/errorHandler: missing arg typing

--- src/lib/http/error-handler.ts
+++ src/lib/http/error-handler.ts
@@ -1,6 +1,6 @@
 import * as process from 'node:process';
 
-export const errorHandler = async (error: any) => {
+export const errorHandler = async (error: Error) => {
 	if (process.env['NODE_ENV'] !== 'test') {
 		// eslint-disable-next-line node/no-unsupported-features/es-syntax
 		const appsignal = await import('../appsignal.js');
