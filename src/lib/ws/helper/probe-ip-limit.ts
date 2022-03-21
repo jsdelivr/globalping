26cfdaa20132957f3aa44722f8260f854b48e27f
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-03-21 08:56:06 +0000

    add: exception for dev mode

--- src/lib/ws/helper/probe-ip-limit.ts
+++ src/lib/ws/helper/probe-ip-limit.ts
@@ -1,3 +1,4 @@
+import * as process from 'node:process';
 import type {Socket} from 'socket.io';
 import {getWsServer, PROBES_NAMESPACE} from '../server.js';
 import {scopedLogger} from '../../logger.js';
@@ -6,6 +7,10 @@ const io = getWsServer();
 const logger = scopedLogger('ws:limit');
 
 export const verifyIpLimit = async (socket: Socket) => {
+	if (process.env['FAKE_PROBE_IP']) {
+		return;
+	}
+
 	const socketList = await io.of(PROBES_NAMESPACE).fetchSockets();
 	const previousSocket = socketList.find(s =>
 		s.data.probe.ipAddress === socket.data['probe'].ipAddress && s.id !== socket.id,
