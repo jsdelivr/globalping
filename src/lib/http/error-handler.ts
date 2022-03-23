c295e278bf3e0124bf2639376042b5576c557376
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-03-23 11:15:48 +0000

    fix: unecessary async keyword

--- src/lib/http/error-handler.ts
+++ src/lib/http/error-handler.ts
@@ -1,3 +1,3 @@
 import appsignal from '../appsignal.js';
 
-export const errorHandler = async (error: Error) => appsignal.tracer().setError(error);
+export const errorHandler = (error: Error) => appsignal.tracer().setError(error);
