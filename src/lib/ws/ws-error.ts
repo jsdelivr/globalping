1f91ff74fe2949b8fd53593487d1d42f03c66835
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-03-24 10:11:36 +0000

    add: ws-error constructor

---
+++ src/lib/ws/ws-error.ts
@@ -0,0 +1,29 @@
+import type {Probe} from '../../probe/types.js';
+
+type Info = {
+	socketId: string;
+	probe?: Probe;
+	cause?: any;
+};
+
+type JsonResponse = {
+	message: string;
+	info: Info;
+};
+
+export class WsError extends Error {
+	info: Info;
+	constructor(message: string, info: Info) {
+		super(message);
+		this.info = info;
+	}
+
+	toJson(): JsonResponse {
+		return {
+			message: this.message,
+			info: this.info,
+		};
+	}
+}
+
+export default WsError;
