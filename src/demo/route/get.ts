551e2b0198a1c7fcb9a8661e6410597720cb2fb5
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-03-29 14:31:06 +0000

    add: serve public files

---
+++ src/demo/route/get.ts
@@ -0,0 +1,14 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import type {DefaultContext, DefaultState, ParameterizedContext} from 'koa';
+import type Router from '@koa/router';
+
+const handle = async (ctx: ParameterizedContext<DefaultState, DefaultContext & Router.RouterParamContext>) => {
+	const {file} = ctx.params;
+	ctx.body = fs.readFileSync(path.join(path.resolve(), '/public/views/', file ?? 'index.html'), 'utf8');
+};
+
+export const registerDemoRoute = (router: Router) => {
+	router.get('/demo/:file', handle);
+	router.get('/demo/', handle);
+};
--- src/lib/http/server.ts
+++ src/lib/http/server.ts
@@ -7,6 +7,7 @@ import responseTime from 'koa-response-time';
 import {registerGetProbesRoute} from '../../probe/route/get-probes.js';
 import {registerGetMeasurementRoute} from '../../measurement/route/get-measurement.js';
 import {registerCreateMeasurementRoute} from '../../measurement/route/create-measurement.js';
+import {registerDemoRoute} from '../../demo/route/get.js';
 import {errorHandler} from './error-handler.js';
 import {rateLimitHandler} from './middleware/ratelimit.js';
 import {errorHandlerMw} from './middleware/error-handler.js';
@@ -23,6 +24,9 @@ registerGetMeasurementRoute(router);
 
 // GET /probes
 registerGetProbesRoute(router);
+
+// GET /demo
+registerDemoRoute(router);
 
 app
 	// Error handler must always be the first middleware in a chain unless you know what you are doing ;)
