b3f73ff242699984614b3e29b760192a8a05b808
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-03-30 11:02:51 +0000

    fix: new probe resposne schema

--- public/probes.vue.js
+++ public/probes.vue.js
@@ -8,9 +8,15 @@ const probes = () => ({
     this.fetchProbes()
   },
   methods: {
+    parsedLocation(index) {
+      const probe = this.probes[index];
+      const city = probe.location.country === 'US' ? `${probe.location.city} (${probe.location.state})` : probe.location.city;
+
+      return `${city}, ${probe.location.country}, ${probe.location.continent}, ${probe.location.asn}`
+    },
     async fetchProbes() {
       const url = '/v1/probes';
-      this.probes = await (await fetch(url)).json()
+      this.probes = await (await fetch(url)).json();
     },
   },
   template: `
@@ -19,8 +25,8 @@ const probes = () => ({
           {{ probes.length }} probes available
         </h2>
         <ul>
-          <li v-for="probe in probes">
-            {{ probe.city }}, {{ probe.country }}
+          <li v-for="(probe, index) in probes">
+            [{{ probe.version }}] {{ parsedLocation(index) }}
           </li>
         </ul>
       </div>
