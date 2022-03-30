f1f86e9d923a989f63424f75b3fd5f3f2cef377b
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-03-30 10:26:29 +0000

    add: measurement/schema: test traceroute schema

--- test/tests/unit/measurement/schema.test.ts
+++ test/tests/unit/measurement/schema.test.ts
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {pingSchema} from '../../../../src/measurement/schema/command-schema.js';
+import {pingSchema, tracerouteSchema} from '../../../../src/measurement/schema/command-schema.js';
 
 describe('command schema', () => {
 	describe('ping', () => {
@@ -25,7 +25,7 @@ describe('command schema', () => {
 			expect(valid.value.type).to.equal('ping');
 		});
 
-		it('should pass (deel equal)', async () => {
+		it('should pass (deep equal)', async () => {
 			const input = {
 				type: 'ping',
 				target: 'abc.com',
@@ -33,6 +33,46 @@ describe('command schema', () => {
 			};
 
 			const valid = pingSchema.validate(input);
+
+			expect(valid.error).to.not.exist;
+			expect(valid.value).to.deep.equal(input);
+		});
+	});
+
+	describe('traceroute', () => {
+		it('should fail (missing values)', async () => {
+			const input = {
+				type: 'traceroute',
+			};
+
+			const valid = tracerouteSchema.validate(input);
+
+			expect(valid.error).to.exist;
+		});
+
+		it('should pass and correct values (incorrect caps)', async () => {
+			const input = {
+				type: 'TRACEroute',
+				target: 'abc.com',
+				protocol: 'udp',
+			};
+
+			const valid = tracerouteSchema.validate(input);
+
+			expect(valid.error).to.not.exist;
+			expect(valid.value.type).to.equal('traceroute');
+			expect(valid.value.protocol).to.equal('UDP');
+		});
+
+		it('should pass (deep equal)', async () => {
+			const input = {
+				type: 'traceroute',
+				target: 'abc.com',
+				protocol: 'TCP',
+				port: 80,
+			};
+
+			const valid = tracerouteSchema.validate(input);
 
 			expect(valid.error).to.not.exist;
 			expect(valid.value).to.deep.equal(input);
