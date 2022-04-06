8b807b39e15cda12148a5d8080c5f264d42fb460
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-04-06 16:26:08 +0100

    fix: json parser: respond with 400 error (#80)

---
+++ src/lib/http/middleware/body-parser.ts
@@ -0,0 +1,8 @@
+import createHttpError from 'http-errors';
+import koaBodyParser from 'koa-bodyparser';
+
+export const bodyParser = () => koaBodyParser({
+	onerror(error) {
+		throw createHttpError(400, error.message);
+	},
+});
