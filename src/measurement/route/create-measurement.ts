6c51173d0724937f2be764f5c8437dcf4af5fb21
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-04-13 23:18:52 +0000

    ref: move measurement/location schema to a seperate file

--- src/measurement/route/create-measurement.ts
+++ src/measurement/route/create-measurement.ts
@@ -2,55 +2,18 @@ import Joi from 'joi';
 import config from 'config';
 import type {Context} from 'koa';
 import type Router from '@koa/router';
-import geoLists from 'countries-list';
 import {getMeasurementRunner} from '../runner.js';
 import type {MeasurementRequest} from '../types.js';
-import {states} from '../../lib/location/states.js';
-import {regions} from '../../lib/location/regions.js';
 import {bodyParser} from '../../lib/http/middleware/body-parser.js';
 import {validate} from '../../lib/http/middleware/validate.js';
 import {dnsSchema, pingSchema, tracerouteSchema} from '../schema/command-schema.js';
+import {schema as locationSchema} from '../schema/location-schema.js';
 
 const runner = getMeasurementRunner();
-const {continents, countries} = geoLists;
 const measurementConfig = config.get<{limits: {global: number; location: number}}>('measurement');
 
-// Todo: better validation. hostname/ip validation for targets
-const schema = Joi.object({
-	locations: Joi.array().items(Joi.object({
-		type: Joi.string().valid('continent', 'region', 'country', 'state', 'city', 'network', 'asn', 'magic').insensitive().required(),
-		value: Joi.alternatives().conditional('type', {
-			switch: [
-				{
-					is: 'continent',
-					then: Joi.string().valid(...Object.keys(continents)).insensitive()
-						.messages({'any.only': 'The continent must be a valid two-letter ISO code'}),
-				},
-				{is: 'region', then: Joi.string().valid(...Object.keys(regions)).insensitive()},
-				{
-					is: 'country',
-					then: Joi.string().valid(...Object.keys(countries)).insensitive()
-						.messages({'any.only': 'The country must be a valid two-letter ISO code'}),
-				},
-				{
-					is: 'state',
-					then: Joi.string().valid(...Object.keys(states)).insensitive()
-						.messages({'any.only': 'The US state must be a valid two-letter code, e.g. CA'}),
-				},
-				{is: 'city', then: Joi.string().min(1).max(128).insensitive()},
-				{is: 'network', then: Joi.string().min(1).max(128).insensitive()},
-				{is: 'asn', then: Joi.number()},
-				{is: 'magic', then: Joi.string().min(1).insensitive()},
-			],
-		}).required().messages({
-			'any.required': 'Location value is required',
-		}),
-		limit: Joi.number().min(1).max(measurementConfig.limits.location).when(Joi.ref('/limit'), {
-			is: Joi.exist(),
-			then: Joi.forbidden().messages({'any.unknown': 'limit per location is not allowed when a global limit is set'}),
-			otherwise: Joi.required().messages({'any.required': 'limit per location required when no global limit is set'}),
-		}),
-	})).default([]),
+export const schema = Joi.object({
+	locations: locationSchema,
 	measurement: Joi.alternatives().try(pingSchema, tracerouteSchema, dnsSchema).required(),
 	limit: Joi.number().min(1).max(measurementConfig.limits.global),
 });
