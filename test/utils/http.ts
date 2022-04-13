bccb573d586b70a061f2099e9f69bea30a7fca06
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-04-13 17:43:19 +0100

    feat: add ip/domain blacklist (#84)
    * add: ip blacklist/validator
    * add: domain blacklist/validator
    * add: install validator package
    * add: malware/ip: test updateList + validate methods
    * add: malware/domain: test updateList + validate methods
    * add: test client/validate method
    * add: implement joiValidate + validatsSync methods
    * add: malware test/util methods
    * add: gitignore: ip/domain blacklists

--- test/utils/http.ts
+++ test/utils/http.ts
@@ -1,9 +1,16 @@
 import type {Server} from 'node:http';
 import {createServer} from '../../src/lib/server.js';
+import {
+	populateIpList,
+	populateDomainList,
+} from './malware.js';
 
 let app: Server;
 
 export const getTestServer = async (): Promise<Server> => {
+	await populateIpList();
+	await populateDomainList();
+
 	if (!app) {
 		app = await createServer();
 		app.listen(0);
