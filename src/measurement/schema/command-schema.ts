b5e46d585eb0b8188d75376ecf6a570564266f54
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-04-25 15:55:34 +0000

    add: custom error messages for malware + private validators

--- src/measurement/schema/command-schema.ts
+++ src/measurement/schema/command-schema.ts
@@ -1,39 +1,47 @@
-import Joi from 'joi';
+import Joi, {CustomHelpers, ErrorReport} from 'joi';
 import isIpPrivate from 'private-ip';
-import {joiValidate as joiMalwareValidate} from '../../lib/malware/client.js';
+import {
+	joiValidate as joiMalwareValidate,
+	joiSchemaErrorMessage as joiMalwareSchemaErrorMessage,
+} from '../../lib/malware/client.js';
 import {joiValidate as joiMalwareValidateIp} from '../../lib/malware/ip.js';
 import {joiValidate as joiMalwareValidateDomain} from '../../lib/malware/domain.js';
 
 export const validCmdTypes = ['ping', 'dns', 'traceroute'];
 
-export const joiValidateTarget = (type: string) => (value: string): string | Error => {
+export const joiValidateTarget = (type: string) => (value: string, helpers: CustomHelpers): string | ErrorReport => {
 	if (['ip', 'any'].includes(type) && isIpPrivate(value)) {
-		throw new Error('ip.private');
+		return helpers.error('ip.private');
 	}
 
 	if (type === 'domain') {
-		return joiMalwareValidateDomain(value);
+		return joiMalwareValidateDomain(value, helpers);
 	}
 
 	if (type === 'ip') {
-		return joiMalwareValidateIp(value);
+		return joiMalwareValidateIp(value, helpers);
 	}
 
-	return joiMalwareValidate(value);
+	return joiMalwareValidate(value, helpers);
+};
+
+export const schemaErrorMessages = {
+	...joiMalwareSchemaErrorMessage(),
+	'ip.private': 'Private hostnames are not allowed.',
 };
 
 export const pingSchema = Joi.object({
 	type: Joi.string().valid('ping').insensitive().required(),
 	target: Joi.alternatives().try(Joi.string().ip(), Joi.string().domain()).custom(joiValidateTarget('any')).required(),
 	packets: Joi.number().min(1).max(16).default(3),
-});
+}).messages(schemaErrorMessages);
 
 export const tracerouteSchema = Joi.object({
 	type: Joi.string().valid('traceroute').insensitive().required(),
 	target: Joi.alternatives().try(Joi.string().ip(), Joi.string().domain()).custom(joiValidateTarget('any')).required(),
 	protocol: Joi.string().valid('TCP', 'UDP', 'ICMP').insensitive().default('ICMP'),
 	port: Joi.number().port().default(80),
-});
+}).messages(schemaErrorMessages);
 
 const allowedTypes = ['A', 'AAAA', 'ANY', 'CNAME', 'DNSKEY', 'DS', 'MX', 'NS', 'NSEC', 'PTR', 'RRSIG', 'SOA', 'TXT', 'SRV'];
 const allowedProtocols = ['UDP', 'TCP'];
@@ -47,4 +55,4 @@ export const dnsSchema = Joi.object({
 		protocol: Joi.string().valid(...allowedProtocols).insensitive().default('UDP'),
 		port: Joi.number().default('53'),
 	}).default({}),
-});
+}).messages(schemaErrorMessages);
