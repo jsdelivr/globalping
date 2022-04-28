1849663250087608ae9d864da8c5ab758cdc885d
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-04-28 15:02:05 +0000

    add: retry logic + report error

--- src/lib/geoip/maxmind.ts
+++ src/lib/geoip/maxmind.ts
@@ -1,11 +1,35 @@
 import config from 'config';
-import {WebServiceClient} from '@maxmind/geoip2-node';
+import {City, WebServiceClient} from '@maxmind/geoip2-node';
+import type {WebServiceClientError} from '@maxmind/geoip2-node/dist/src/types';
+import appsignal from '../appsignal.js';
 import {LocationInfo, normalizeCityName} from './client.js';
 
 const client = new WebServiceClient(config.get('maxmind.accountId'), config.get('maxmind.licenseKey'));
 
+export const isMaxmindError = (error: any): error is WebServiceClientError => (error as WebServiceClientError).code !== undefined;
+
+const query = async (addr: string, retryCounter = 0): Promise<City | undefined> => {
+	try {
+		const data = await client.city(addr);
+		return data;
+	} catch (error: unknown) {
+		if (isMaxmindError(error)) {
+			if (error.code === 'SERVER_ERROR' && retryCounter < 3) {
+				return query(addr, retryCounter + 1);
+			}
+
+			if (error.code === 'ACCOUNT_ID_REQUIRED') {
+				appsignal.tracer().sendError(new Error(error.error), span => {
+					span.setName('geoip.lookup');
+					span.set('client', 'maxmind');
+				});
+			}
+		}
+	}
+};
+
 export const maxmindLookup = async (addr: string): Promise<LocationInfo> => {
-	const data = await client.city(addr);
+	const data = await query(addr);
 
 	if (!data) {
 		throw new Error('no maxmind data');
