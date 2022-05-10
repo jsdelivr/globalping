45842008c5553e7a63c05010663a60caa340eaec
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-05-10 21:00:48 +0100

    geoip improvements (#114)
    * fix: filter out empty fields
    
    * add: use maxmind network + asn OR default match on fallback
    
    * fix: existing tests: asn
    
    * add: test incomplete sources fallback

--- src/lib/geoip/client.ts
+++ src/lib/geoip/client.ts
@@ -14,7 +14,7 @@ export const normalizeCityName = (string_: string): string => anyAscii(string_).
 export const normalizeNetworkName = (string_: string): string => string_.toLowerCase();
 
 const bestMatch = (field: keyof LocationInfo, sources: LocationInfo[]): LocationInfo => {
-	const ranked = Object.values(_.groupBy(sources, field)).sort((a, b) => b.length - a.length).flat();
+	const ranked = Object.values(_.groupBy(sources.filter(s => s[field]), field)).sort((a, b) => b.length - a.length).flat();
 	const best = ranked[0];
 
 	if (!best) {
@@ -61,15 +61,16 @@ export const geoIpLookup = async (addr: string): Promise<LocationInfo> => {
 		}) as LocationInfo[];
 
 	const match = bestMatch('city', results);
+	const maxmindMatch = results[2];
 
 	return {
 		continent: match.continent,
 		country: match.country,
 		state: match.state,
 		city: match.city,
-		asn: Number(match.asn),
+		asn: Number(maxmindMatch?.asn ?? match.asn),
 		latitude: Number(match.latitude),
 		longitude: Number(match.longitude),
-		network: normalizeNetworkName(match.network),
+		network: normalizeNetworkName(maxmindMatch?.network ?? match.network),
 	};
 };
