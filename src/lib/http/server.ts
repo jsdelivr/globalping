5cfd93bf7245a51a85c8bead6ffd448255f7355b
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-07-12 05:32:14 +0000

    ref: http: json-prettify by default

--- src/lib/http/server.ts
+++ src/lib/http/server.ts
@@ -52,7 +52,7 @@ app
 	.use(rateLimitHandler())
 	.use(responseTime())
 	.use(cors())
-	.use(json({pretty: false, param: 'pretty'}))
+	.use(json({pretty: true, spaces: 2}))
 	.use(apiRouter.routes())
 	.use(apiRouter.allowedMethods());
 
