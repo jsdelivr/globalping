169c399831a8af07356ef0f20d9d4c2e8c81254c
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-07-20 13:25:27 +0000

    fix: ipinfo: handle potentially missing fields

--- src/lib/geoip/providers/ipinfo.ts
+++ src/lib/geoip/providers/ipinfo.ts
@@ -5,11 +5,11 @@ import type {LocationInfo} from '../client.js';
 import {normalizeCityName} from '../utils.js';
 
 type IpinfoResponse = {
-	country: string;
-	city: string;
-	region: string;
+	country: string | undefined;
+	city: string | undefined;
+	region: string | undefined;
 	org: string | undefined;
-	loc: string;
+	loc: string | undefined;
 };
 
 export const ipinfoLookup = async (addr: string): Promise<LocationInfo> => {
@@ -18,16 +18,16 @@ export const ipinfoLookup = async (addr: string): Promise<LocationInfo> => {
 		timeout: {request: 5000},
 	}).json<IpinfoResponse>();
 
-	const [lat, lon] = (result.loc || ',').split(',');
+	const [lat, lon] = (result.loc ?? ',').split(',');
 	const match = /^AS(\d+)/.exec(result.org ?? '');
 	const parsedAsn = match?.[1] ? Number(match[1]) : null;
 	const network = (result.org ?? '').split(' ').slice(1).join(' ');
 
 	return {
-		continent: getContinentByCountry(result.country),
-		state: result.country === 'US' ? getStateIsoByName(result.region) : undefined,
-		country: result.country,
-		city: normalizeCityName(result.city),
+		continent: getContinentByCountry(result.country ?? ''),
+		state: result.country === 'US' ? getStateIsoByName(result.region ?? '') : undefined,
+		country: result.country ?? '',
+		city: normalizeCityName(result.city ?? ''),
 		asn: parsedAsn!,
 		latitude: Number(lat),
 		longitude: Number(lon),
