e2524c2941961ae60672619a0b870d6f933db61e
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-07-26 15:06:37 +0000

    ref: remove type-based error matching

--- src/lib/http/middleware/validate.ts
+++ src/lib/http/middleware/validate.ts
@@ -1,26 +1,11 @@
 import type {Schema} from 'joi';
 import type {Context, Next} from 'koa';
-import {validCmdTypes} from '../../../measurement/schema/command-schema.js';
-
-type DeepFieldDetails = {
-	message: string;
-	context: {
-		message: string;
-	};
-	path: string[];
-};
 
 export const validate = (schema: Schema) => async (ctx: Context, next: Next) => {
 	const valid = schema.validate(ctx.request.body, {convert: true});
 
 	if (valid.error) {
-		const deepErrorMatch = valid.error.details
-			.filter(field => field?.context!['details'])
-			.flatMap(field => (field.context!['details'] as DeepFieldDetails))
-			.filter(item => item && !(item.path.includes('type') && validCmdTypes.includes(valid.error._original.measurement.type)));
-
-		const finalError = deepErrorMatch.length > 0 ? deepErrorMatch : valid.error.details;
-		const fields = finalError.map(field => ([field.path.join('.'), String(field.context?.message || field.message)]));
+		const fields = valid.error.details.map(field => ([field.path.join('.'), String(field?.message)]));
 
 		ctx.status = 422;
 		ctx.body = {
