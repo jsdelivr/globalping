1cdf6ac5b83bf478d4e8dcedf29c9988bef3dca3
DecliningLotus <declininglotus@gmail.com>
2022-08-03 15:21:29 +0300

    refactor: cities varname

--- src/lib/geoip/altnames.ts
+++ src/lib/geoip/altnames.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/naming-convention, quote-props */
-export const altNames: Record<string, string> = {
+export const cities: Record<string, string> = {
 	'Geneve': 'Geneva',
 	'Frankfurt am Main': 'Frankfurt',
 	'New York City': 'New York',
--- src/lib/geoip/utils.ts
+++ src/lib/geoip/utils.ts
@@ -1,10 +1,10 @@
 import anyAscii from 'any-ascii';
-import {altNames} from './altnames.js';
+import {cities} from './altnames.js';
 
 export const normalizeCityNamePublic = (name: string): string => {
 	// We don't add city to the regex as there are valid names like 'Mexico City' or 'Kansas City'
 	const asciiName = anyAscii(name).replace(/(?:\s+|^)the(?:\s+|$)/gi, '');
-	return altNames[asciiName] ?? asciiName;
+	return cities[asciiName] ?? asciiName;
 };
 
 export const normalizeCityName = (name: string): string => normalizeCityNamePublic(name).toLowerCase();
