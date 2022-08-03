ba75b3083d5eb81522cb8c75787cf91f51b4d370
DecliningLotus <declininglotus@gmail.com>
2022-08-03 13:33:22 +0300

    perf: use obj instead of map

--- src/lib/geoip/utils.ts
+++ src/lib/geoip/utils.ts
@@ -1,12 +1,17 @@
 import anyAscii from 'any-ascii';
 
-const altNames: Array<[string, string]> = [['Geneve', 'Geneva'], ['Frankfurt am Main', 'Frankfurt'], ['New York City', 'New York']];
-const altNamesMap = new Map<string, string>(altNames);
+/* eslint-disable @typescript-eslint/naming-convention */
+const altNames: Record<string, string> = {
+	Geneve: 'Geneva',
+	'Frankfurt am Main': 'Frankfurt',
+	'New York City': 'New York',
+};
+/* eslint-enable @typescript-eslint/naming-convention */
 
 export const normalizeCityNamePublic = (name: string): string => {
 	// We don't add city to the regex as there are valid names like 'Mexico City' or 'Kansas City'
 	const asciiName = anyAscii(name).replace(/(?:\s+|^)the(?:\s+|$)/gi, '');
-	return altNamesMap.get(asciiName) ?? asciiName;
+	return altNames[asciiName] ?? asciiName;
 };
 
 export const normalizeCityName = (name: string): string => normalizeCityNamePublic(name).toLowerCase();
