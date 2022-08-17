e8ce3bb23acb908d31a7e59fae07d078e85d13dd
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-08-17 10:42:19 +0000

    ref: hide sensitive info behind admin key

---
+++ src/lib/http/middleware/is-admin.ts
@@ -0,0 +1,8 @@
+import type {Context, Next} from 'koa';
+import config from 'config';
+
+export const isAdminMw = async (ctx: Context, next: Next) => {
+	const adminKey = config.get<string>('admin.key');
+	ctx['isAdmin'] = adminKey.length > 0 && ctx.query['adminkey'] === adminKey;
+	return next();
+};
