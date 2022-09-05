f782d96ac974ade07e6607d6ba0b33f19261b402
Patryk Cieszkowski <patryk.cieszkowski@gmail.com>
2022-09-05 12:14:05 +0000

    ref: replace domain validator with validator pkg

--- src/measurement/schema/command-schema.ts
+++ src/measurement/schema/command-schema.ts
@@ -4,6 +4,7 @@ import {
 	joiSchemaErrorMessage as joiMalwareSchemaErrorMessage,
 } from '../../lib/malware/client.js';
 import {
+	joiValidateDomain,
 	joiValidateTarget,
 	whenTypeApply,
 	globalIpOptions,
@@ -13,6 +14,7 @@ import {
 export const schemaErrorMessages = {
 	...joiMalwareSchemaErrorMessage(),
 	'ip.private': 'Private hostnames are not allowed.',
+	'domain.invalid': 'Provided target is not a valid domain name',
 };
 /* eslint-enable @typescript-eslint/naming-convention */
 
@@ -21,7 +23,13 @@ export const validCmdTypes = ['ping', 'dns', 'traceroute', 'mtr', 'http'];
 const allowedHttpProtocols = ['http', 'https', 'http2'];
 const allowedHttpMethods = ['get', 'head'];
 
-const httpTargetSchema = Joi.alternatives().try(Joi.string().ip(globalIpOptions), Joi.string().domain()).custom(joiValidateTarget('any')).required();
+// Http
+const httpTargetSchema = Joi.alternatives()
+	.try(Joi.string().ip(globalIpOptions), Joi.custom(joiValidateDomain()))
+	.custom(joiValidateTarget('any'))
+	.required()
+	.messages(schemaErrorMessages);
+
 export const httpSchema = Joi.object({
 	request: Joi.object({
 		method: Joi.string().valid(...allowedHttpMethods).insensitive().default('head'),
@@ -35,7 +43,13 @@ export const httpSchema = Joi.object({
 	port: Joi.number(),
 }).default();
 
-const mtrTargetSchema = Joi.alternatives().try(Joi.string().ip(globalIpOptions), Joi.string().domain()).custom(joiValidateTarget('any')).required();
+// Mtr
+const mtrTargetSchema = Joi.alternatives()
+	.try(Joi.string().ip(globalIpOptions), Joi.custom(joiValidateDomain()))
+	.custom(joiValidateTarget('any'))
+	.required()
+	.messages(schemaErrorMessages);
+
 const allowedMtrProtocols = ['UDP', 'TCP', 'ICMP'];
 export const mtrSchema = Joi.object({
 	protocol: Joi.string().valid(...allowedMtrProtocols).insensitive().default('ICMP'),
@@ -43,12 +57,24 @@ export const mtrSchema = Joi.object({
 	port: Joi.number().port().default(80),
 }).default();
 
-const pingTargetSchema = Joi.alternatives().try(Joi.string().ip(globalIpOptions), Joi.string().domain()).custom(joiValidateTarget('any')).required();
+// Ping
+const pingTargetSchema = Joi.alternatives()
+	.try(Joi.string().ip(globalIpOptions), Joi.custom(joiValidateDomain()))
+	.custom(joiValidateTarget('any'))
+	.required()
+	.messages(schemaErrorMessages);
+
 export const pingSchema = Joi.object({
 	packets: Joi.number().min(1).max(16).default(3),
 }).default().messages(schemaErrorMessages);
 
-const tracerouteTargetSchema = Joi.alternatives().try(Joi.string().ip(globalIpOptions), Joi.string().domain()).custom(joiValidateTarget('any')).required();
+const tracerouteTargetSchema = Joi.alternatives()
+	.try(Joi.string().ip(globalIpOptions), Joi.custom(joiValidateDomain()))
+	.custom(joiValidateTarget('any'))
+	.required()
+	.messages(schemaErrorMessages);
+
+// Traceroute
 export const tracerouteSchema = Joi.object({
 	protocol: Joi.string().valid('TCP', 'UDP', 'ICMP').insensitive().default('ICMP'),
 	port: Joi.number().port().default(80),
@@ -57,8 +83,9 @@ export const tracerouteSchema = Joi.object({
 const allowedDnsTypes = ['A', 'AAAA', 'ANY', 'CNAME', 'DNSKEY', 'DS', 'MX', 'NS', 'NSEC', 'PTR', 'RRSIG', 'SOA', 'TXT', 'SRV'];
 const allowedDnsProtocols = ['UDP', 'TCP'];
 
-const dnsDefaultTargetSchema = Joi.string().domain().custom(joiValidateTarget('domain')).required();
-const dnsPtrTargetSchema = Joi.string().ip(globalIpOptions).custom(joiValidateTarget('ip')).required();
+// Dns
+const dnsDefaultTargetSchema = Joi.custom(joiValidateDomain()).custom(joiValidateTarget('domain')).required().messages(schemaErrorMessages);
+const dnsPtrTargetSchema = Joi.string().ip(globalIpOptions).custom(joiValidateTarget('ip')).required().messages(schemaErrorMessages);
 const dnsTargetSchema = Joi.when(Joi.ref('..measurementOptions.query.type'), {is: Joi.string().insensitive().valid('PTR').required(), then: dnsPtrTargetSchema, otherwise: dnsDefaultTargetSchema});
 export const dnsSchema = Joi.object({
 	query: Joi.object({
