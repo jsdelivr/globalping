a15bf9184caf0b44a115063de6fb40b5dd63c14f
Alexey Yarmosh <yarmoshalexey@gmail.com>
2023-04-06 13:34:21 +0200

    feat: use clustering on the API side

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import process from 'node:process';
+import cluster from 'node:cluster';
+import physicalCpuCount from 'physical-cpu-count';
 import { scopedLogger } from './lib/logger.js';
 import { createServer } from './lib/server.js';
 
-const logger = scopedLogger('global');
+const logger = scopedLogger('index');
 const port = process.env['PORT'] ?? 3000;
 
 const workerFn = async () => {
@@ -13,6 +15,21 @@ const workerFn = async () => {
 	});
 };
 
-workerFn().catch((error) => {
-	logger.error('failed to start cluster', error);
-});
+if (cluster.isPrimary) {
+	logger.info(`Master ${process.pid} is running with ${physicalCpuCount} workers`);
+
+	for (let i = 0; i < physicalCpuCount; i++) {
+		cluster.fork();
+	}
+
+	cluster.on('exit', (worker) => {
+		logger.error(`worker ${worker.process.pid!} died`);
+		cluster.fork();
+	});
+} else {
+	logger.info(`Worker ${process.pid} is running`);
+
+	workerFn().catch((error) => {
+		logger.error('failed to start cluster', error);
+	});
+}
