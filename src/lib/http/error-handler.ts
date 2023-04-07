7aa1a73b7f19ff1b89a71ae4d4ec117b897a533c
Alexey Yarmosh <yarmoshalexey@gmail.com>
2023-04-07 09:11:23 +0200

    feat: ignore econnreset errors in newrelic

--- src/lib/http/error-handler.ts
+++ src/lib/http/error-handler.ts
@@ -3,7 +3,13 @@ import { scopedLogger } from '../logger.js';
 
 const logger = scopedLogger('error-handler-http');
 
-export const errorHandler = (error: unknown) => {
+export const errorHandler = (error: Error & {code?: string}) => {
+	const ignore = [ 'ECONNABORTED', 'ECONNRESET', 'EPIPE' ];
+
+	if (error?.code && ignore.includes(error.code)) {
+		return;
+	}
+
 	if (error instanceof Error) {
 		newrelic.noticeError(error);
 	}
