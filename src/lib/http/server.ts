ffaf0b205ab7e7ea738d73c2e98a754d5d97af67
Martin Kolárik <martin@kolarik.sk>
2023-04-19 00:03:06 +0200

    fix: use koa-static for /demo

--- src/lib/http/server.ts
+++ src/lib/http/server.ts
@@ -1,16 +1,17 @@
 import { createServer } from 'node:http';
 import * as zlib from 'node:zlib';
+import * as url from 'node:url';
 import json from 'koa-json';
 import Router from '@koa/router';
 import conditionalGet from 'koa-conditional-get';
 import compress from 'koa-compress';
 import etag from 'koa-etag';
 import responseTime from 'koa-response-time';
+import koaStatic from 'koa-static';
 import cjsDependencies from '../../cjs-dependencies.cjs';
 import { registerGetProbesRoute } from '../../probe/route/get-probes.js';
 import { registerGetMeasurementRoute } from '../../measurement/route/get-measurement.js';
 import { registerCreateMeasurementRoute } from '../../measurement/route/create-measurement.js';
-import { registerDemoRoute } from '../../demo/route/get.js';
 import { registerHealthRoute } from '../../health/route/get.js';
 import { errorHandler } from './error-handler.js';
 import { rateLimitHandler } from './middleware/ratelimit.js';
@@ -35,7 +36,11 @@ rootRouter.get('/', (ctx) => {
 });
 
 const apiRouter = new Router({ strict: true, sensitive: true });
-apiRouter.prefix('/v1');
+
+apiRouter.prefix('/v1')
+	.use(isAdminMw)
+	.use(rateLimitHandler());
+
 // POST /measurements
 registerCreateMeasurementRoute(apiRouter);
 // GET /measurements/:id
@@ -43,33 +48,25 @@ registerGetMeasurementRoute(apiRouter);
 // GET /probes
 registerGetProbesRoute(apiRouter);
 
-const demoRouter = new Router({ strict: true, sensitive: true });
-demoRouter.prefix('/demo');
-// GET /demo
-registerDemoRoute(demoRouter);
-
 const healthRouter = new Router({ strict: true, sensitive: true });
 // GET /health
 registerHealthRoute(healthRouter);
 
 app
+	.use(responseTime())
 	.use(domainRedirect())
 	.use(compress({ br: { params: { [zlib.constants.BROTLI_PARAM_QUALITY]: 4 } }, gzip: { level: 3 }, deflate: false }))
 	.use(conditionalGet())
 	.use(etag({ weak: true }))
-// Exclude root + demo routers from any checks
-	.use(rootRouter.routes())
-	.use(demoRouter.routes())
-	.use(healthRouter.routes())
+	.use(json({ pretty: true, spaces: 2 }))
 	// Error handler must always be the first middleware in a chain unless you know what you are doing ;)
 	.use(errorHandlerMw)
-	.use(isAdminMw)
-	.use(rateLimitHandler())
-	.use(responseTime())
 	.use(corsHandler())
-	.use(json({ pretty: true, spaces: 2 }))
+	.use(rootRouter.routes())
+	.use(healthRouter.routes())
 	.use(apiRouter.routes())
-	.use(apiRouter.allowedMethods());
+	.use(apiRouter.allowedMethods())
+	.use(koaStatic(url.fileURLToPath(new URL('.', import.meta.url)) + '/../../../public', { format: false }));
 
 app.on('error', errorHandler);
 
