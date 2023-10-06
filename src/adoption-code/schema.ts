9c7cd7799ebc1a866551f8855265bccb6390e8d1
Martin Kolárik <martin@kolarik.sk>
2023-10-06 21:58:53 +0200

    fix: IP validation

--- src/adoption-code/schema.ts
+++ src/adoption-code/schema.ts
@@ -1,6 +1,7 @@
 import Joi from 'joi';
+import { globalIpOptions } from '../measurement/schema/utils.js';
 
 export const schema = Joi.object({
-	ip: Joi.string().ip().required(),
+	ip: Joi.string().ip(globalIpOptions).required(),
 	code: Joi.string().length(6).required(),
 });
