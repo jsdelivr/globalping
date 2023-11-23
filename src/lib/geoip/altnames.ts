94e709678b81f1a855d2acb8e46672ae7ea8a5db
Alexey Yarmosh <yarmoshalexey@gmail.com>
2023-11-23 13:26:50 +0100

    feat: add new cities altnames

--- src/lib/geoip/altnames.ts
+++ src/lib/geoip/altnames.ts
@@ -2,4 +2,6 @@ export const cities: Record<string, string> = {
 	'Geneve': 'Geneva',
 	'Frankfurt am Main': 'Frankfurt',
 	'New York City': 'New York',
+	'Santiago de Queretaro': 'Queretaro',
+	'Nurnberg': 'Nuremberg',
 };
