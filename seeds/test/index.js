1f50d49d7a89ca4f0fcd0109c3d51acfd7b75007
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-01-09 15:29:45 +0100

    feat: remove unused seeds

--- seeds/test/index.js
+++ seeds/test/index.js
@@ -1,6 +1,2 @@
-export const seed = async (db) => {
-	await db('directus_users').insert({
-		id: '89da69bd-a236-4ab7-9c5d-b5f52ce09959',
-		github: 'jimaek',
-	});
+export const seed = async () => {
 };
