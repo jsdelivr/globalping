26dddab43ec202c045470762b8fb9dbcbdb17115
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-01-26 13:32:42 +0100

    feat: add int tests

--- test/tests/integration/ratelimit.test.ts
+++ test/tests/integration/ratelimit.test.ts
@@ -4,7 +4,7 @@ import requestIp from 'request-ip';
 import { expect } from 'chai';
 import { getTestServer, addFakeProbe, deleteFakeProbes } from '../../utils/server.js';
 import nockGeoIpProviders from '../../utils/nock-geo-ip.js';
-import { anonymousRateLimiter } from '../../../src/lib/rate-limiter.js';
+import { anonymousRateLimiter, authenticatedRateLimiter } from '../../../src/lib/rate-limiter.js';
 
 describe('rate limiter', () => {
 	let app: Server;
@@ -34,6 +34,7 @@ describe('rate limiter', () => {
 
 	afterEach(async () => {
 		await anonymousRateLimiter.delete(clientIpv6);
+		await authenticatedRateLimiter.delete('89da69bd-a236-4ab7-9c5d-b5f52ce09959');
 	});
 
 	after(async () => {
@@ -68,6 +69,19 @@ describe('rate limiter', () => {
 			expect(response.headers['x-ratelimit-reset']).to.exist;
 		});
 
+		it('should include headers (authenticated POST)', async () => {
+			const response = await requestAgent.post('/v1/measurements')
+				.set('Authorization', 'Bearer v2lUHEVLtVSskaRKDBabpyp4AkzdMnob')
+				.send({
+					type: 'ping',
+					target: 'jsdelivr.com',
+				}).expect(202) as Response;
+
+			expect(response.headers['x-ratelimit-limit']).to.exist;
+			expect(response.headers['x-ratelimit-remaining']).to.exist;
+			expect(response.headers['x-ratelimit-reset']).to.exist;
+		});
+
 		it('should change values on multiple requests (POST)', async () => {
 			const response = await requestAgent.post('/v1/measurements').send({
 				type: 'ping',
@@ -87,9 +101,33 @@ describe('rate limiter', () => {
 			expect(response2.headers['x-ratelimit-remaining']).to.equal('99998');
 			expect(response2.headers['x-ratelimit-reset']).to.equal('3600');
 		});
+
+		it('should change values on multiple requests (authenticated POST)', async () => {
+			const response = await requestAgent.post('/v1/measurements')
+				.set('Authorization', 'Bearer v2lUHEVLtVSskaRKDBabpyp4AkzdMnob')
+				.send({
+					type: 'ping',
+					target: 'jsdelivr.com',
+				}).expect(202) as Response;
+
+			expect(response.headers['x-ratelimit-limit']).to.equal('250');
+			expect(response.headers['x-ratelimit-remaining']).to.equal('249');
+			expect(response.headers['x-ratelimit-reset']).to.equal('3600');
+
+			const response2 = await requestAgent.post('/v1/measurements')
+				.set('Authorization', 'Bearer v2lUHEVLtVSskaRKDBabpyp4AkzdMnob')
+				.send({
+					type: 'ping',
+					target: 'jsdelivr.com',
+				}).expect(202) as Response;
+
+			expect(response2.headers['x-ratelimit-limit']).to.equal('250');
+			expect(response2.headers['x-ratelimit-remaining']).to.equal('248');
+			expect(response2.headers['x-ratelimit-reset']).to.equal('3600');
+		});
 	});
 
-	describe('access', () => {
+	describe('anonymous access', () => {
 		it('should succeed (limit not reached)', async () => {
 			await anonymousRateLimiter.set(clientIpv6, 0, 0);
 
@@ -120,6 +158,48 @@ describe('rate limiter', () => {
 				target: 'jsdelivr.com',
 				limit: 2,
 			}).expect(429) as Response;
+
+			expect(Number(response.headers['x-ratelimit-remaining'])).to.equal(1);
+		});
+	});
+
+	describe('authenticated access', () => {
+		it('should succeed (limit not reached)', async () => {
+			await authenticatedRateLimiter.set('89da69bd-a236-4ab7-9c5d-b5f52ce09959', 0, 0);
+
+			const response = await requestAgent.post('/v1/measurements')
+				.set('Authorization', 'Bearer v2lUHEVLtVSskaRKDBabpyp4AkzdMnob')
+				.send({
+					type: 'ping',
+					target: 'jsdelivr.com',
+				}).expect(202) as Response;
+
+			expect(Number(response.headers['x-ratelimit-remaining'])).to.equal(249);
+		});
+
+		it('should fail (limit reached) (start at 100)', async () => {
+			await authenticatedRateLimiter.set('89da69bd-a236-4ab7-9c5d-b5f52ce09959', 250, 0);
+
+			const response = await requestAgent.post('/v1/measurements')
+				.set('Authorization', 'Bearer v2lUHEVLtVSskaRKDBabpyp4AkzdMnob')
+				.send({
+					type: 'ping',
+					target: 'jsdelivr.com',
+				}).expect(429) as Response;
+
+			expect(Number(response.headers['x-ratelimit-remaining'])).to.equal(0);
+		});
+
+		it('should consume all points successfully or none at all (cost > remaining > 0)', async () => {
+			await authenticatedRateLimiter.set('89da69bd-a236-4ab7-9c5d-b5f52ce09959', 249, 0); // 1 remaining
+
+			const response = await requestAgent.post('/v1/measurements')
+				.set('Authorization', 'Bearer v2lUHEVLtVSskaRKDBabpyp4AkzdMnob')
+				.send({
+					type: 'ping',
+					target: 'jsdelivr.com',
+					limit: 2,
+				}).expect(429) as Response;
 
 			expect(Number(response.headers['x-ratelimit-remaining'])).to.equal(1);
 		});
