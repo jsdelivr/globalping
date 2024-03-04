aa5bc34b6a4a664b77c25f4a54d8c066277edcc3
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-03-04 14:19:14 +0100

    feat: run probe container from e2e setup

--- test-e2e/setup.ts
+++ test-e2e/setup.ts
@@ -1,3 +1,38 @@
+import Docker from 'dockerode';
+import { setTimeout } from 'timers/promises';
+
+let container: Docker.Container;
+
 before(async () => {
-	console.log('run docker container');
+	const docker = new Docker();
+
+	container = await docker.createContainer({
+		Image: 'ghcr.io/jsdelivr/globalping-probe',
+		name: 'globalping-probe-e2e',
+		Env: [
+			'API_HOST=ws://host.docker.internal:3000',
+		],
+		HostConfig: {
+			LogConfig: {
+				Type: 'local',
+				Config: {},
+			},
+			NetworkMode: 'host',
+		},
+	});
+
+	await container.start({});
+
+	const stream = await container.logs({
+		follow: true,
+		stdout: true,
+		stderr: true,
+	});
+	container.modem.demuxStream(stream, process.stdout, process.stderr);
+
+	await setTimeout(5000);
+});
+
+after(async () => {
+	await container.remove({ force: true });
 });
