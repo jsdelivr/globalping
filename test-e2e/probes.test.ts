9a1e134fc96ddf667b8762f28ecc3557249ba4db
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-03-05 16:48:39 +0100

    feat: add util parser

--- test-e2e/probes.test.ts
+++ test-e2e/probes.test.ts
@@ -1,13 +1,20 @@
 import got from 'got';
 import { expect } from 'chai';
+import supertest from 'supertest';
+
+const gotResponseToSupertestResponse = (gotResponse) => {
+	const supertestResponse = { ...gotResponse } as unknown as supertest.Response;
+	supertestResponse.body = JSON.parse(gotResponse.body);
+	supertestResponse.type = gotResponse.headers['content-type'] === 'application/json; charset=utf-8' ? 'application/json' : gotResponse.headers['content-type'];
+	return supertestResponse;
+};
 
 describe('/probes endpoint', () => {
 	it('should return an array of probes', async () => {
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		const response = await got('http://127.0.0.1:3000/v1/probes') as any;
-		response.body = JSON.parse(response.body);
-		response.type = response.headers['content-type'] === 'application/json; charset=utf-8' ? 'application/json' : response.headers['content-type'];
+		const gotResponse = await got('http://localhost:3000/v1/probes');
+		const response = gotResponseToSupertestResponse(gotResponse);
 
+		expect(response.statusCode).to.equal(200);
 		expect(response.body.length).to.equal(1);
 		expect(response).to.matchApiSchema();
 	});
