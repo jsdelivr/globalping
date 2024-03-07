962a447488eeec3da753790c72c488286e78b485
Alexey Yarmosh <yarmoshalexey@gmail.com>
2024-03-07 12:08:02 +0100

    feat: add locations e2e

---
+++ test-e2e/tests/location.test.ts
@@ -0,0 +1,86 @@
+import got from 'got';
+import { expect } from 'chai';
+
+describe('locations filter', () => {
+	it('should create measurement without location', async () => {
+		const response = await got.post('http://localhost:3000/v1/measurements', { json: {
+			target: 'www.jsdelivr.com',
+			type: 'ping',
+		} });
+
+		expect(response.statusCode).to.equal(202);
+	});
+
+	it('should create measurement by valid "city" value', async () => {
+		const response = await got.post('http://localhost:3000/v1/measurements', { json: {
+			target: 'www.jsdelivr.com',
+			type: 'ping',
+			locations: [{
+				city: 'Paris',
+			}],
+		} });
+
+		expect(response.statusCode).to.equal(202);
+	});
+
+	it('should not create measurement by invalid "city" value', async () => {
+		const response = await got.post('http://localhost:3000/v1/measurements', { json: {
+			target: 'www.jsdelivr.com',
+			type: 'ping',
+			locations: [{
+				city: 'Ouagadougou',
+			}],
+		}, throwHttpErrors: false });
+
+		expect(response.statusCode).to.equal(422);
+	});
+
+	it('should create measurement by valid "magic" value', async () => {
+		const response = await got.post('http://localhost:3000/v1/measurements', { json: {
+			target: 'www.jsdelivr.com',
+			type: 'ping',
+			locations: [{
+				magic: 'Paris',
+			}],
+		} });
+
+		expect(response.statusCode).to.equal(202);
+	});
+
+	it('should not create measurement by invalid "magic" value', async () => {
+		const response = await got.post('http://localhost:3000/v1/measurements', { json: {
+			target: 'www.jsdelivr.com',
+			type: 'ping',
+			locations: [{
+				magic: 'Ouagadougou',
+			}],
+		}, throwHttpErrors: false });
+
+		expect(response.statusCode).to.equal(422);
+	});
+
+	it('should create measurement by id of another measurement', async () => {
+		const { id } = await got.post('http://localhost:3000/v1/measurements', { json: {
+			target: 'www.jsdelivr.com',
+			type: 'ping',
+		} }).json();
+
+		const response = await got.post('http://localhost:3000/v1/measurements', { json: {
+			target: 'www.jsdelivr.com',
+			type: 'ping',
+			locations: id,
+		} });
+
+		expect(response.statusCode).to.equal(202);
+	});
+
+	it('should not create measurement wrong id of another measurement', async () => {
+		const response = await got.post('http://localhost:3000/v1/measurements', { json: {
+			target: 'www.jsdelivr.com',
+			type: 'ping',
+			locations: 'wrongIdValue',
+		}, throwHttpErrors: false });
+
+		expect(response.statusCode).to.equal(422);
+	});
+});
