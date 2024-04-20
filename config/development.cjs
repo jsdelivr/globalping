a741f56a1ed02ced69a5c4c3d8f8d43291fb7ea5
Martin Kolárik <martin@kolarik.sk>
2024-04-20 17:50:20 +0200

    refactor: remap ports (#517)

--- config/development.cjs
+++ config/development.cjs
@@ -1,7 +1,13 @@
 module.exports = {
 	redis: {
+		url: 'redis://localhost:16379',
 		socket: {
 			tls: false,
+		},
+	},
+	db: {
+		connection: {
+			port: 13306,
 		},
 	},
 	admin: {
--- config/test.cjs
+++ config/test.cjs
@@ -1,11 +1,13 @@
 module.exports = {
 	redis: {
+		url: 'redis://localhost:16379',
 		socket: {
 			tls: false,
 		},
 	},
 	db: {
 		connection: {
+			port: 13306,
 			database: 'directus-test',
 			multipleStatements: true,
 		},
