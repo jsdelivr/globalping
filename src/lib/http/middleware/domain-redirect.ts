ed65b15e8c54a737fdd25f4089a99f3cc6e1023a
Martin Kolárik <martin@kolarik.sk>
2024-09-25 16:19:49 +0200

    fix: preserve query string in web redirects

--- src/lib/http/middleware/domain-redirect.ts
+++ src/lib/http/middleware/domain-redirect.ts
@@ -3,7 +3,7 @@ import type { Context, Next } from 'koa';
 const domainRedirect = () => (ctx: Context, next: Next) => {
 	if (ctx.host === 'globalping.io') {
 		ctx.status = 301;
-		ctx.redirect(`https://jsdelivr.com/globalping${ctx.path !== '/' ? ctx.path : ''}`);
+		ctx.redirect(`https://jsdelivr.com/globalping${ctx.path !== '/' ? ctx.url : ctx.url.substring(1)}`);
 		return;
 	}
 
