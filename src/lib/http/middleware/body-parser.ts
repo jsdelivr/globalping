2dfb6ef45e2f3bb5338af53f7b35add78581e0e9
Martin Kolárik <martin@kolarik.sk>
2024-10-19 02:35:22 +0200

    perf: set json body limit to 100kb

--- src/lib/http/middleware/body-parser.ts
+++ src/lib/http/middleware/body-parser.ts
@@ -2,6 +2,8 @@ import createHttpError from 'http-errors';
 import koaBodyParser from 'koa-bodyparser';
 
 export const bodyParser = () => koaBodyParser({
+	enableTypes: [ 'json' ],
+	jsonLimit: '100kb',
 	onerror (error) {
 		throw createHttpError(400, error.message);
 	},
