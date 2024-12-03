c7138c8ca98af25c097db08238b65de633dc4b02
Martin Kolárik <martin@kolarik.sk>
2024-12-03 15:24:04 +0100

    test: add IPv6 e2e tests (#584)

--- test/e2e/cases/http.test.ts
+++ test/e2e/cases/http.test.ts
@@ -36,6 +36,42 @@ describe('http measurement', () => {
 		expect(response).to.matchApiSchema();
 	});
 
+	it('should finish successfully in case of IPv6 domain target', async () => {
+		const { id } = await got.post('http://localhost:80/v1/measurements', { json: {
+			target: 'www.jsdelivr.com',
+			type: 'http',
+			measurementOptions: {
+				ipVersion: 6,
+			},
+		} }).json<any>();
+
+		const response = await waitMeasurementFinish(id);
+
+		expect(response.body.status).to.equal('finished');
+		expect(response.body.results[0].result.status).to.equal('finished');
+		expect(response.body.results[0].result.rawBody).to.equal(null);
+		expect(response).to.matchApiSchema();
+	});
+
+	it('should finish successfully in case of IPv6 address target', async () => {
+		const { id } = await got.post('http://localhost:80/v1/measurements', { json: {
+			target: '2606:4700:3037::ac43:d071',
+			type: 'http',
+			measurementOptions: {
+				request: {
+					host: 'www.jsdelivr.com',
+				},
+			},
+		} }).json<any>();
+
+		const response = await waitMeasurementFinish(id);
+
+		expect(response.body.status).to.equal('finished');
+		expect(response.body.results[0].result.status).to.equal('finished');
+		expect(response.body.results[0].result.rawBody).to.equal(null);
+		expect(response).to.matchApiSchema();
+	});
+
 	it('should return 400 for blacklisted target', async () => {
 		const response = await got.post('http://localhost:80/v1/measurements', { json: {
 			target: 'dpd.96594345154.xyz',
