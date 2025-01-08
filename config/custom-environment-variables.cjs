8a6a05400aeea86de0b5230b23c6a455ced9eb58
Martin Kolárik <martin@kolarik.sk>
2025-01-08 15:27:11 +0100

    refactor: switch to config-mapper-env

--- config/custom-environment-variables.cjs
+++ config/custom-environment-variables.cjs
@@ -1,25 +1,2 @@
-const _ = require('lodash');
 const df = require('./default.cjs');
-
-function mapEnvConfig (object, prefix = '') {
-	return _.mapValues(object, (value, key) => {
-		const currentKey = (prefix ? `${prefix}_` : '') + _.snakeCase(key).toUpperCase();
-
-		if (_.isObject(value)) {
-			return mapEnvConfig(value, currentKey);
-		}
-
-		if (typeof value === 'number' || typeof value === 'boolean') {
-			return {
-				__name: currentKey,
-				__format: typeof value,
-			};
-		}
-
-		return currentKey;
-	});
-}
-
-const mapped = mapEnvConfig(df);
-
-module.exports = mapped;
+module.exports = require('config-mapper-env')(df);
