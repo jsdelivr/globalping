46ed56b4a8c9e55fd672909d703e318a724a2197
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-02-04 14:53:51 +0100

    feat: add dns result schema

--- src/measurement/handler/result.ts
+++ src/measurement/handler/result.ts
@@ -1,11 +1,11 @@
 import Joi from 'joi';
 import type { Probe } from '../../probe/types.js';
-import type { MeasurementResultMessage, PingResult, TracerouteResult } from '../types.js';
+import type { DnsRegularResult, DnsTraceResult, MeasurementResultMessage, PingResult, TestResult, TracerouteResult } from '../types.js';
 import { getMeasurementRunner } from '../runner.js';
 import { getProbeValidator } from '../../lib/probe-validator.js';
 
 const pingResultSchema = Joi.object<PingResult>({
-	status: Joi.string().required(),
+	status: Joi.string().valid('finished', 'failed').required(),
 	rawOutput: Joi.string().required(),
 	resolvedAddress: Joi.string().allow(null),
 	resolvedHostname: Joi.string().allow(null),
@@ -25,7 +25,7 @@ const pingResultSchema = Joi.object<PingResult>({
 });
 
 const tracerouteResultSchema = Joi.object<TracerouteResult>({
-	status: Joi.string().required(),
+	status: Joi.string().valid('finished', 'failed').required(),
 	rawOutput: Joi.string().required(),
 	resolvedAddress: Joi.string().allow(null),
 	resolvedHostname: Joi.string().allow(null),
@@ -38,6 +38,49 @@ const tracerouteResultSchema = Joi.object<TracerouteResult>({
 	})),
 });
 
+const dnsResultSchema = Joi.alternatives([
+	Joi.object<TestResult & DnsRegularResult>({
+		status: Joi.string().valid('finished', 'failed').required(),
+		rawOutput: Joi.string().required(),
+		statusCodeName: Joi.string().required(),
+		statusCode: Joi.number().allow(null).required(),
+		resolver: Joi.string().allow(null).required(),
+		timings: Joi.object({
+			total: Joi.number().allow(null),
+		}).required(),
+		answers: Joi.array().items(Joi.alternatives([
+			Joi.object({
+				name: Joi.string().allow(null),
+				type: Joi.string().allow(null),
+				ttl: Joi.number(),
+				class: Joi.string().allow(null),
+				value: Joi.string(),
+			}),
+			Joi.object({}),
+		])).required(),
+	}),
+	Joi.object<TestResult & DnsTraceResult>({
+		status: Joi.string().valid('finished', 'failed').required(),
+		rawOutput: Joi.string().required(),
+		hops: Joi.array().items(Joi.object({
+			resolver: Joi.string().allow(null).required(),
+			timings: Joi.object({
+				total: Joi.number().allow(null),
+			}).required(),
+			answers: Joi.array().items(Joi.alternatives([
+				Joi.object({
+					name: Joi.string().allow(null),
+					type: Joi.string().allow(null),
+					ttl: Joi.number(),
+					class: Joi.string().allow(null),
+					value: Joi.string(),
+				}),
+				Joi.object({}),
+			])).required(),
+		})),
+	}),
+]);
+
 const schema = Joi.object<MeasurementResultMessage>({
 	testId: Joi.string().required(),
 	measurementId: Joi.string().required(),
@@ -45,6 +88,7 @@ const schema = Joi.object<MeasurementResultMessage>({
 	result: Joi.alternatives([
 		pingResultSchema,
 		tracerouteResultSchema,
+		dnsResultSchema,
 	]).required(),
 }).required();
 
