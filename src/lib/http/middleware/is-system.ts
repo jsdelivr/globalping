4bd9ef8ad79c490ff07464e1bdb1999cec619df6
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-03-20 12:02:59 +0100

    feat: move is-system auth to headers

--- src/lib/http/middleware/is-system.ts
+++ src/lib/http/middleware/is-system.ts
@@ -4,16 +4,6 @@ import config from 'config';
 const systemKey = config.get<string>('systemApi.key');
 
 export const isSystemMw: Middleware = async (ctx, next) => {
-	ctx['isSystem'] = false;
-	const authorization = ctx.headers.authorization;
-
-	if (authorization) {
-		const parts = authorization.split(' ');
-
-		if (parts.length === 2 && parts[0] === 'Bearer' && parts[1] === systemKey) {
-			ctx['isSystem'] = true;
-		}
-	}
-
+	ctx['isSystem'] = ctx.headers['x-api-key'] === systemKey;
 	return next();
 };
