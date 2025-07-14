bf67d9286d2d4f87495a5a92e7b7db14de20af41
Martin Kolárik <martin@kolarik.sk>
2025-07-14 19:05:53 +0200

    fix: request body reporting (#683)

--- src/lib/http/middleware/body-parser.ts
+++ src/lib/http/middleware/body-parser.ts
@@ -1,10 +1,26 @@
-import createHttpError from 'http-errors';
+import { IncomingMessage } from 'node:http';
 import koaBodyParser from 'koa-bodyparser';
+import createHttpError from 'http-errors';
+import { ExtendedMiddleware } from '../../../types.js';
 
-export const bodyParser = () => koaBodyParser({
-	enableTypes: [ 'json' ],
-	jsonLimit: '100kb',
-	onerror (error) {
-		throw createHttpError(400, error.message);
-	},
-});
+interface RequestWithBody extends IncomingMessage {
+	body?: unknown;
+}
+
+export const bodyParser = (): ExtendedMiddleware => {
+	const parser = koaBodyParser({
+		enableTypes: [ 'json' ],
+		jsonLimit: '100kb',
+		onerror (error) {
+			throw createHttpError(400, error.message);
+		},
+	});
+
+	return async (ctx, next) => {
+		await parser(ctx, async () => {
+			// Elastic APM expects this on the underlying request.
+			(ctx.req as RequestWithBody).body = ctx.request.body;
+			await next();
+		});
+	};
+};
