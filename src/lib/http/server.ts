beaf47a18fe1db6ae3406782a51ae5bb67c24268
Martin Kolárik <martin@kolarik.sk>
2025-07-16 16:55:33 +0200

    fix: reduce APM data usage

--- src/lib/http/server.ts
+++ src/lib/http/server.ts
@@ -35,6 +35,21 @@ apmAgent.addTransactionFilter(apmUtils.transactionFilter({
 	keepResponse: [ 'location' ],
 }));
 
+apmAgent.addTransactionFilter((payload) => {
+	if (!payload['context']) {
+		return payload;
+	}
+
+	const request = (payload['context'] as { request?: { body?: string; url: URL } }).request;
+
+	// Store only 10% of measurement request bodies.
+	if (request?.url.pathname === '/v1/measurements' && request?.body && Math.random() > 0.1) {
+		delete request.body;
+	}
+
+	return payload;
+});
+
 // Filter out short SPUBLISH spans.
 apmAgent.addSpanFilter((payload) => {
 	if (payload['type'] !== 'db' || payload['subtype'] !== 'redis' || ![ 'SPUBLISH' ].includes(payload['name'] as string)) {
