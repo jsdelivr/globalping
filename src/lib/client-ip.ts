99a4bfe4037f47f22fe69b4758780068f3b76ff2
Martin Kolárik <martin@kolarik.sk>
2025-08-11 16:45:51 +0200

    fix: ensure IPs are only resolved from trusted proxies via config

--- src/lib/client-ip.ts
+++ src/lib/client-ip.ts
@@ -1,10 +1,18 @@
 import { IncomingMessage } from 'node:http';
 import proxyaddr from 'proxy-addr';
+import config from 'config';
 
 const ipv4MappedPattern = /^::ffff:/i;
 
+const trustedProxies = config.get<string[]>('server.trustedProxies');
+const trustPredicate = proxyaddr.compile([
+	...trustedProxies,
+	'loopback',
+	'linklocal',
+]);
+
 export const getIpFromRequest = (request: IncomingMessage) => {
-	const ip = proxyaddr(request, (_address, index) => index < 1);
+	const ip = proxyaddr(request, trustPredicate);
 
 	if (!ip) {
 		return ip;
