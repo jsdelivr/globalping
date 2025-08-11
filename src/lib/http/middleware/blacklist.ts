90641e81596a17a94ea47d3215c8b71a2459e5c0
Pavel Kopecký <pavel.kopecky@icloud.com>
2025-08-11 18:15:04 +0200

    fix: block access based on IP only

--- src/lib/http/middleware/blacklist.ts
+++ src/lib/http/middleware/blacklist.ts
@@ -1,29 +1,20 @@
 import type { Context, Next } from 'koa';
 import { validate as validateIP } from '../../malware/ip.js';
-import { validate as validateDomain } from '../../malware/domain.js';
-
-
-function setBlacklistContext (ctx: Context, source: string) {
-	ctx.status = 403;
-
-	ctx.body = {
-		error: {
-			type: 'access_forbidden',
-			message: `Access from ${source} has been forbidden for security reasons.`,
-		},
-	};
-}
 
 export const blacklist = async (ctx: Context, next: Next) => {
 	const ip = ctx.request.ip;
-	const hostname = ctx.request.hostname;
 
 	if (!validateIP(ip)) {
-		return setBlacklistContext(ctx, ip);
-	}
+		ctx.status = 403;
 
-	if (hostname && hostname !== ip && !validateDomain(hostname)) {
-		return setBlacklistContext(ctx, hostname);
+		ctx.body = {
+			error: {
+				type: 'access_forbidden',
+				message: `Access from ${ip} has been forbidden for security reasons.`,
+			},
+		};
+
+		return;
 	}
 
 	await next();
--- test/tests/unit/middleware/blacklist.test.ts
+++ test/tests/unit/middleware/blacklist.test.ts
@@ -5,15 +5,11 @@ import path from 'node:path';
 import nock from 'nock';
 
 import { sourceList as ipSourceList, updateList as updateListIp } from '../../../../src/lib/malware/ip.js';
-import { sourceList as domainSourceList, updateList as updateListDomain } from '../../../../src/lib/malware/domain.js';
 import { getTestServer } from '../../../utils/server.js';
 import type { Server } from 'node:http';
 
 const mockDataPath = path.join(path.resolve(), 'test/mocks/malware');
-
 const ipMockResult = await readFile(path.join(mockDataPath, 'nock-ip.txt'), 'utf8');
-const domainMockResult = await readFile(path.join(mockDataPath, 'nock-domain.txt'), 'utf8');
-
 
 describe('blacklist middleware', () => {
 	let app: Server;
@@ -25,13 +21,7 @@ describe('blacklist middleware', () => {
 			nock(url.origin).get(url.pathname).reply(200, ipMockResult);
 		}
 
-		for (const source of domainSourceList) {
-			const url = new URL(source);
-			nock(url.origin).get(url.pathname).reply(200, domainMockResult);
-		}
-
 		await updateListIp();
-		await updateListDomain();
 
 		app = await getTestServer();
 		requestAgent = request(app);
@@ -41,63 +31,23 @@ describe('blacklist middleware', () => {
 		nock.cleanAll();
 	});
 
-	const makeRequest = (xff: string, host?: string) => {
+	const makeRequest = (xff: string) => {
 		const req = requestAgent.get('/');
-
-		if (host) {
-			req.set('Host', host);
-		}
 
 		return req.set('X-Forwarded-For', xff);
 	};
 
-	describe('ip', () => {
-		it('should pass (ok ip)', async () => {
-			const res = await makeRequest('127.0.0.1');
-			expect(res.status).to.not.equal(403);
-		});
-
-		it('should fail (blacklisted ip)', async () => {
-			const res = await makeRequest('100.33.75.218');
-
-			expect(res.status).to.equal(403);
-			expect(res.body?.error).to.exist;
-			expect(res.body.error.type).to.equal('access_forbidden');
-			expect(res.body.error.message).to.equal('Access from 100.33.75.218 has been forbidden for security reasons.');
-		});
-
-		it('should fail (blacklisted ip, ok domain)', async () => {
-			const res = await makeRequest('100.33.75.218', 'google.com');
-
-			expect(res.status).to.equal(403);
-			expect(res.body?.error).to.exist;
-			expect(res.body.error.type).to.equal('access_forbidden');
-			expect(res.body.error.message).to.equal('Access from 100.33.75.218 has been forbidden for security reasons.');
-		});
-
-		it('should fail (blacklisted ip, blacklisted domain)', async () => {
-			const res = await makeRequest('100.33.75.218', 'webdesignme.xyz');
-
-			expect(res.status).to.equal(403);
-			expect(res.body?.error).to.exist;
-			expect(res.body.error.type).to.equal('access_forbidden');
-			expect(res.body.error.message).to.equal('Access from 100.33.75.218 has been forbidden for security reasons.');
-		});
+	it('should pass (ok ip)', async () => {
+		const res = await makeRequest('127.0.0.1');
+		expect(res.status).to.not.equal(403);
 	});
 
-	describe('domain', () => {
-		it('should pass (ok domain)', async () => {
-			const res = await makeRequest('127.0.0.1', 'google.com');
-			expect(res.status).to.not.equal(403);
-		});
+	it('should fail (blacklisted ip)', async () => {
+		const res = await makeRequest('100.33.75.218');
 
-		it('should fail (blacklisted domain)', async () => {
-			const res = await makeRequest('127.0.0.1', 'utenze-app-2022.net');
-
-			expect(res.status).to.equal(403);
-			expect(res.body?.error).to.exist;
-			expect(res.body.error.type).to.equal('access_forbidden');
-			expect(res.body.error.message).to.equal('Access from utenze-app-2022.net has been forbidden for security reasons.');
-		});
+		expect(res.status).to.equal(403);
+		expect(res.body?.error).to.exist;
+		expect(res.body.error.type).to.equal('access_forbidden');
+		expect(res.body.error.message).to.equal('Access from 100.33.75.218 has been forbidden for security reasons.');
 	});
 });
