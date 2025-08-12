d0e9a973c7aee858af90d10048185a2331e5d1b1
Pavel Kopecký <pavel.kopecky@icloud.com>
2025-08-12 18:21:20 +0200

    refactor: use createHttpError in the blacklist middleware

--- src/lib/http/middleware/blacklist.ts
+++ src/lib/http/middleware/blacklist.ts
@@ -1,20 +1,12 @@
 import type { Context, Next } from 'koa';
 import { validate as validateIP } from '../../malware/ip.js';
+import createHttpError from 'http-errors';
 
 export const blacklist = async (ctx: Context, next: Next) => {
 	const ip = ctx.request.ip;
 
 	if (!validateIP(ip)) {
-		ctx.status = 403;
-
-		ctx.body = {
-			error: {
-				type: 'access_forbidden',
-				message: `Access from ${ip} has been forbidden for security reasons.`,
-			},
-		};
-
-		return;
+		throw createHttpError(403, `Access from ${ip} has been forbidden for security reasons.`, { type: 'access_forbidden' });
 	}
 
 	await next();
