f00452045a30e977fd2468685547eb521ea368c8
Alexey Yarmosh <yarmoshalexey@gmail.com>
2025-09-23 13:33:16 +0200

    feat: add rate limiter

--- src/alternative-ip/route/alternative-ip.ts
+++ src/alternative-ip/route/alternative-ip.ts
@@ -1,14 +1,37 @@
-import type { Context } from 'koa';
 import type Router from '@koa/router';
 import createHttpError from 'http-errors';
+import { RateLimiterMemory, RateLimiterRes } from 'rate-limiter-flexible';
 import { getAltIpsClient } from '../../lib/alt-ips-client.js';
+import type { ExtendedContext } from '../../types.js';
+import { getIdFromRequest } from '../../lib/rate-limiter/get-id-from-request.js';
 
-const handle = async (ctx: Context): Promise<void> => {
+export const rateLimiter = new RateLimiterMemory({
+	points: 100,
+	duration: 60,
+});
+
+export const checkRateLimit = async (ctx: ExtendedContext) => {
+	const clientId = getIdFromRequest(ctx.request);
+
+	try {
+		await rateLimiter.consume(clientId);
+	} catch (error) {
+		if (error instanceof RateLimiterRes) {
+			throw createHttpError(429, `Too many requests.`, { type: 'too_many_requests' });
+		}
+
+		throw createHttpError(500);
+	}
+};
+
+const handle = async (ctx: ExtendedContext): Promise<void> => {
 	const ip = ctx.request.ip;
 
 	if (!ip) {
 		throw createHttpError(400, 'Unable to get requester ip.', { type: 'no_ip' });
 	}
+
+	await checkRateLimit(ctx);
 
 	const token = await getAltIpsClient().generateToken(ip);
 	ctx.body = { ip, token };
