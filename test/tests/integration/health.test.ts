39c579be9d42cdf3cd937acc1d302fd42383eb2b
Alex Yarmosh <yarmoshalexey@gmail.com>
2026-01-21 18:23:42 +0100

    fix: pass SIGTERM and SIGINT to workers (#779)

--- test/tests/integration/health.test.ts
+++ test/tests/integration/health.test.ts
@@ -1,27 +1,15 @@
 import type { Server } from 'node:http';
-import process from 'node:process';
 import { expect } from 'chai';
-import * as sinon from 'sinon';
 import request, { type Agent } from 'supertest';
 import { getTestServer } from '../../utils/server.js';
-
-after(() => {
-	process.removeAllListeners('SIGTERM');
-	process.removeAllListeners('SIGINT');
-});
 
 describe('Get health', () => {
 	let app: Server;
 	let requestAgent: Agent;
-	const sandbox = sinon.createSandbox();
 
 	before(async () => {
 		app = await getTestServer();
 		requestAgent = request(app);
-	});
-
-	afterEach(() => {
-		sandbox.restore();
 	});
 
 	describe('health endpoint', () => {
