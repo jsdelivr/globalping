ddc10c20f3d79912f1aab363edd485e375a50947
Martin Kolárik <martin@kolarik.sk>
2026-03-05 15:49:52 +0100

    fix: use allowed directly

--- src/schedule/executor.ts
+++ src/schedule/executor.ts
@@ -171,7 +171,7 @@ export class StreamScheduleExecutor {
 			}
 		}
 
-		return localProbes.filter(probe => allowed.has(probe));
+		return [ ...allowed.values() ];
 	}
 
 	private emitMeasurementToProbe (
