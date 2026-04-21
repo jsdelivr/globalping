f8aabf8642452bdedbe5c74c9110938cc862cc11
Martin Kolárik <martin@kolarik.sk>
2026-04-21 09:17:24 +0200

    feat: add cache-control rules

--- src/lib/http/middleware/default-headers.ts
+++ src/lib/http/middleware/default-headers.ts
@@ -1,7 +1,8 @@
 import type { Middleware } from 'koa';
 
 export const defaultHeaders = (): Middleware => async (ctx, next) => {
-	ctx.set('Cache-Control', 'no-store, no-cache, must-revalidate');
+	ctx.set('Cache-Control', 'private, no-store, no-cache, must-revalidate');
+	ctx.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
 	ctx.set('X-Robots-Tag', 'noindex');
 
 	await next();
--- src/lib/http/server.ts
+++ src/lib/http/server.ts
@@ -157,7 +157,13 @@ export const getHttpServer = (ioContext: IoContext) => {
 		.use(apiRouter.routes())
 		.use(apiRouter.allowedMethods())
 		.use(koaElasticUtils.middleware(apmAgent))
-		.use(koaStatic(publicPath, { format: false }));
+		.use(koaStatic(publicPath, {
+			format: false,
+			maxage: 60_000,
+			setHeaders: (res) => {
+				res.setHeader('Cache-Control', 'public, max-age=60, stale-while-revalidate=60, stale-if-error=86400');
+			},
+		}));
 
 	app.on('error', errorHandler);
 
