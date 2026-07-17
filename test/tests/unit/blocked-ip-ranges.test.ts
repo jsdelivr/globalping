84fb398d5b929c16684bcc0e2002ec27ef14ce75
Pavel Kopecký <pavel.kopecky@icloud.com>
2026-07-17 12:45:47 +0200

    perf: use ip-range-list for range lookups (#872)

--- test/tests/unit/blocked-ip-ranges.test.ts
+++ test/tests/unit/blocked-ip-ranges.test.ts
@@ -1,6 +1,5 @@
 import { expect } from 'chai';
-import { isIpBlocked, blockedRangesIPv4, populateMemList } from '../../../src/lib/blocked-ip-ranges.js';
-import ipaddr from 'ipaddr.js';
+import { isIpBlocked, populateMemList } from '../../../src/lib/blocked-ip-ranges.js';
 
 describe('blocked-ip-ranges', () => {
 	after(async () => {
@@ -16,14 +15,6 @@ describe('blocked-ip-ranges', () => {
 		it('should check IPv6 ranges', () => {
 			expect(isIpBlocked('2a02:26f7:b000:4000::0001')).to.equal(true);
 			expect(isIpBlocked('2a02:26f7:1337:4000::0001')).to.equal(false);
-		});
-
-		it('should cache results', () => {
-			expect(isIpBlocked('172.224.226.1')).to.equal(true);
-			expect(isIpBlocked('172.228.226.1')).to.equal(false);
-			blockedRangesIPv4.add(ipaddr.parseCIDR('172.228.226.1/27'));
-			expect(isIpBlocked('172.224.226.1')).to.equal(true);
-			expect(isIpBlocked('172.228.226.1')).to.equal(false);
 		});
 	});
 });
